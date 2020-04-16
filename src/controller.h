778e1cd9caf567dbc9f0069e6a2cc6ab09be3892
Mustafa TUNCER <tuncerm@gmail.com>
2020-04-16 23:13:27 +0200

    Initial commit

---
+++ src/controller.h
@@ -0,0 +1,16 @@
+//Copied From CppND-Capstone-Snake-Game
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include "character.h"
+
+class Controller {
+ public:
+  void HandleInput(bool &running, Snake &snake) const;
+
+ private:
+  void ChangeDirection(Snake &snake, Snake::Direction input,
+                       Snake::Direction opposite) const;
+};
+
+#endif
