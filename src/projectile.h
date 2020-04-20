b229d633e50400326f47bf12449a051ab96bdce1
Mustafa TUNCER <tuncerm@gmail.com>
2020-04-20 09:13:02 +0200

    Feat: Added projectile entitiy.
    Added a new entity for projectiles. Planning to fead them
    to threads so that they can move on their own.

---
+++ src/projectile.h
@@ -0,0 +1,15 @@
+//Copied From CppND-Capstone-Snake-Game
+#ifndef PROJECTILE_H
+#define PROJECTILE_H
+
+#include <vector>
+#include "character.h"
+#include "gamemap.h"
+
+class Projectile : public Character {
+public:
+    Projectile(int grid_size, int startX, int startY, Direction direction, int speed, std::shared_ptr<GameMap> map_ptr) :
+        Character(grid_size, startX, startY, direction, speed, map_ptr) {}
+};
+
+#endif
